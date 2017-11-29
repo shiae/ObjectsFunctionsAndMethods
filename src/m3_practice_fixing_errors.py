61cf4f723583b782ca53712d9b936c069f05c4ec
Allison Shi <shiae@rose-hulman.edu>
2017-11-29 12:02:52 -0500

    m3 DONE 1

--- src/m3_practice_fixing_errors.py
+++ src/m3_practice_fixing_errors.py
@@ -2,8 +2,8 @@
 This module lets you practice correcting SYNTAX (notation) errors.
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Allison Shi.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ########################################################################
 # TODO: 2.
